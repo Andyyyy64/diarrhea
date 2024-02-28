6ecafa2a3527aaebe00b164f90720cb3348abfe2
andy <takuyarossi77@gmail.com>
2024-02-28 23:57:29 +0900

    use env instead of macro and change audio command to ffplay

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "include/checkdir.h"
 
-#define WATCH_DIR "/home/andy/PROJECT/diarrhea/.git" // directory to be watched
-
 int main() {
-    set_dir(WATCH_DIR); // set the directory to be watched
+
+    char *watch_dir = getenv("WATCH_DIR");
+    set_dir(watch_dir); // set the directory to be watched
     start_checkdir(); // start the thread to watch the directory
     return 0;
 }
