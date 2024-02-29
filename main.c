8894d81a1b2a8c73168c3bbba4f73ec5a009380a
andy <takuyarossi77@gmail.com>
2024-02-29 18:09:32 +0900

    fix file path

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
     char *watch_dir = getenv("WATCH_DIR"); // get the directory to be watched from the environment variable
     char *sound_file = getenv("SOUND_FILE"); // get the sound file from the environment variable
     if (sound_file == NULL) {
-        sound_file = "sound/diarrhea.mp3";
+        sound_file = "sounds/diarrhea.mp3";
     }
     set_dir(watch_dir); // set the directory to be watched
     set_sound(sound_file); // set the sound file to be played
